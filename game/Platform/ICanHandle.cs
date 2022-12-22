043f8897e54a798eba83e5b017a658586c4bc007
morshul <morshu0@proton.me>
2022-12-22 15:27:12 -0300

    refactor: delete all

--- game/Platform/ICanHandle.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Israel Calebe <morshu0@proton.me>. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-namespace minecraft.Game.Platform;
-
-public interface ICanHandle<out T>
-{
-    T Handle { get; }
-}
--- game/Program.cs
+++
@@ -1,4 +0,0 @@
-// Copyright (c) Israel Calebe <morshu0@proton.me>. Licensed under the MIT Licence.
-// See the LICENCE file in the repository root for full licence text.
-
-Console.WriteLine("Hello, world!");
