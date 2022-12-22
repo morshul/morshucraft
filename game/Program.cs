29ffdf92b90290aca1ba311724ee0192a6549df5
morshul <morshu0@proton.me>
2022-12-22 15:25:49 -0300

    refactor: delete all

--- game/Program.cs
+++ game/Program.cs
@@ -1,12 +1,4 @@
 // Copyright (c) Israel Calebe <morshu0@proton.me>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-namespace minecraft.Game;
-
-public static class Program
-{
-    public static void Main(string[] args)
-    {
-        new Application().Run();
-    }
-}
+Console.WriteLine("Hello, world!");
